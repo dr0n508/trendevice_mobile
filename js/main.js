f63175324e701aa718d9fa7f8f7995d5685f767e
Andriy Kornienko <kornienko.andriy@gmail.com>
2016-09-28 21:08:06 +0300

    done homeIphone6

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
 
-    $('.slider-about-us').bxSlider({
-        slideWidth: 220,
-        minSlides: 1,
-        maxSlides: 5,
-        moveSlides: 1,
-        slideMargin: 10,
-        pager: false,
-        infiniteLoop: false
-    });
-
-    $('.offer-for-you').bxSlider({
-        slideWidth: 380,
-        minSlides: 1,
-        maxSlides: 3,
-        moveSlides: 1,
-        slideMargin: 10,
-        pager: false,
-        infiniteLoop: false
-    });
+//    $('.slider-about-us').bxSlider({
+//        slideWidth: 220,
+//        minSlides: 1,
+//        maxSlides: 5,
+//        moveSlides: 1,
+//        slideMargin: 10,
+//        pager: false,
+//        infiniteLoop: false
+//    });
+//
+//    $('.offer-for-you').bxSlider({
+//        slideWidth: 380,
+//        minSlides: 1,
+//        maxSlides: 3,
+//        moveSlides: 1,
+//        slideMargin: 10,
+//        pager: false,
+//        infiniteLoop: false
+//    });
 
     $(".radio-custom-color-input").change(function() {
         $('.selected-color').empty();
